bb9106cfc25b8bb34ae0a93f3033d7e3ee93fb8f
qzpzd <qzpzd1227@163.com>
2022-08-17 12:07:15 +0800

    权重文件

---
+++ inference.sh
@@ -0,0 +1,4 @@
+cd export CUDA_VISIBLE_DEVICES=0
+export PYTHONPATH="/home/disk/tanjing/ambacaffe/python":"."
+/home/disk/tanjing/anaconda3/envs/py38/bin/python /home/disk/qizhongpei/ssd_pytorch/OMS_phone/phone_detection.py --model_type caffe #--croped_pic false --show_video true
+# --Lowrank true --Binary true 
